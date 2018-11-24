148190ab33fd5520e2cf1660d68c4facf04b0157
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-24 02:22:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays(string){
+  return 'Happy Holidays!'
+}
+
+function happyHolidaysTo(string){
+  var name = "you"
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(string){
+  var name = "you"
+  var holiday = "Holiday"
+  return `Happy ${holiday}, ${name}!`
+}
+
